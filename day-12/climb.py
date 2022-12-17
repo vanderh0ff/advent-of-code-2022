62f63e7a5d7f07aef1a48c33042a86de37f61880
matthew vander hoff <m.vanderhoff94@gmail.com>
2022-12-16 20:50:46 -0500

    add distance and start on pushing moves to a stack

--- day-12/climb.py
+++ day-12/climb.py
@@ -29,9 +29,20 @@ def get_moves(x,y, heightmap):
                 if ord(heightmap[pos_y][pos_x]) - ord(heightmap[y][x]) <= 1:
                     possible_moves.append((pos_x,pos_y))
 
+def get_distance(p1, p2):
+    dx = p1[0] - p2[0]
+    dy = p1[1] - p2[1]
+    return math.sqrt(dx ** 2 + dy ** 2)
 
 
 
 
-heightmap = make_heightmap("day-12/tets.txt")
+
+
+starting_conditions = make_heightmap("day-12/tets.txt")
+heightmap = starting_conditions['heightmap']
+current_postion = starting_conditions['start']
+destination = starting_conditions['dest']
+
+for move in get_moves(current_postion[0], current_postion[1], heightmap):
 
