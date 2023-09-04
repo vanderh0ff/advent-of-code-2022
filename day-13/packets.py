cdf63132743cc5472fd58e7f7bee2a517f97b9a4
Matthew Vander Hoff <m.vanderhoff94@gmail.com>
2023-09-04 17:04:33 -0400

    day 13 part 1

--- day-13/packets.py
+++ day-13/packets.py
@@ -1,43 +1,79 @@
 # aoc day 13
 import json
 import logging
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
 def get_packet_pairs(file_location):
     with open(file_location) as input:
         packets = input.read()
         return packets.split('\n\n')
 
-def compare(packet1 , packet2):
-    if type(packet1) == type(packet2):
-        if type(packet1) == type(1):
-            if packet1 > packet2:
-                return False
-            if packet1 < packet2:
-                return True
-        if type(packet1) == type([]):
-            if len(packet1) == 0:
-                return True
-            if len(packet2) == 0:
-                return False
-            if packet1[0] == packet2[0]:
-                return compare(packet1[1:], packet2[1:])
-            if type(packet1[0]) == type(packet2[0]):
-                if type(packet1[0]) == type(1):
-                    if packet1[0] < packet2[0]:
-                        return True
-                    if packet1[0] == packet2[0]:
-                        return compare(packet1[1:], packet2[1:])
-                    if packet1[0] > packet2[0]:
-                        return False
-    else:
-        log.debug(f'compare type mismatch {type(packet1)} and {type(packet2)}')
-        if type(packet1) == type(1):
-            compare([packet1], packet2)
-        else:
-            compare(packet1, [packet2])
 
+def compare(packet1 , packet2):
+    logging.debug(f'compare types {type(packet1)} and {type(packet2)}')
+    logging.debug(f'compare values {packet1} and {packet2}')
+    for i in range(len(packet1)):
+        if i == len(packet2):
+            return False
+        if type(packet1[i]) == type(packet2[i]):
+            if type(packet1[i]) == type(1):
+                logging.debug(f'packet1 is an int {packet1[i]}')
+                if packet1[i] > packet2[i]:
+                    return False
+                if packet1[i] < packet2[i]:
+                    return True
+            if type(packet1[i]) == type([]):
+                logging.debug(f'packet1 is a list {packet1[i]}')
+                if not compare(packet1[i],packet2[i]):
+                    return False
+                if len(packet1[i]) == 0:
+                    return True
+                if len(packet2[i]) == 0:
+                    return False
+        else:
+            if type(packet1[i]) == type(1):
+                if not compare([packet1[i]],packet2[i]):
+                    logging.debug(f'compare values {packet1} and {packet2}')
+                    return False
+            if type(packet1[i]) == type([]):
+                if not compare(packet1[i],[packet2[i]]):
+                    logging.debug(f'compare values {packet1} and {packet2}')
+                    return False
+    return True
+
+
+
+
+#def compare(packet1 , packet2):
+#    logging.debug(f'compare types {type(packet1)} and {type(packet2)}')
+#    logging.debug(f'compare values {packet1} and {packet2}')
+#    if type(packet1) == type(packet2):
+#        if type(packet1) == type(1):
+#            logging.debug(f'packet1 is an int {packet1}')
+#            if packet1 > packet2:
+#                return False
+#            if packet1 < packet2:
+#                return True
+#        if type(packet1) == type([]):
+#            logging.debug(f'packet1 is a list {packet1}')
+#            if len(packet1) == 0:
+#                return True
+#            if len(packet2) == 0:
+#                return False
+#            if packet1[0] == packet2[0]:
+#                return compare(packet1[1:], packet2[1:])
+#            if type(packet1[0]) == type(packet2[0]):
+#                logging.debug(f'packet subelements {packet1[0]} vs {packet2[0]}')
+#                if type(packet1[0]) == type(1):
+#                    if packet1[0] < packet2[0]:
+#                        return True
+#                    if packet1[0] == packet2[0]:
+#                        return compare(packet1[1:], packet2[1:])
+#                    if packet1[0] > packet2[0]:
+#                        return False
+#                else:
+#                    logging.debug(f'compairing nested arrays')
+#                    return compare(packet1[0], packet2[0])
 
 def compare_packets(packets):
     [packet1, packet2] = packets.strip().split('\n')
